417d0c7d527c2b2aa25653f1ec9f22e42a03f719
vigli-Lee <wnsqjadl@gmail.com>
2022-07-08 21:21:19 +0900

    add ViewBinding

--- app/src/main/java/com/vigli/deepdive/example/fragments/MainActivity.kt
+++ app/src/main/java/com/vigli/deepdive/example/fragments/MainActivity.kt
@@ -2,10 +2,19 @@ package com.vigli.deepdive.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vigli.deepdive.example.fragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
